e1227a7b344b7ecc11fe0c89f754413764511527
LuMN <wiwi777guyy@gmail.com>
2024-01-11 09:01:40 +0630

    Showing last message

--- php/data.php
+++ php/data.php
@@ -1,6 +1,8 @@
 <?php
-
+include_once('config.php');
 while($row = $state->fetch()){
+    $db = config();
+    $statement = $db->prepare("SELECT * FROM messages WHERE (receiving_id = {$row['user_id']}) OR (outgoing_id = {$row['user_id']}) ORDER BY msg_id DESC LIMIT 1");
     $output .= '<a href="chat-area.php?user_id='. $row['user_id']  .'">
                 <div class="content">
                     <img src="php/images/'. $row['image'] .'" alt="">
